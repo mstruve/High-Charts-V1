95853da9d7df351e2168c43f02cdc43ded047823
Molly Struve <mollylbs@gmail.com>
2012-09-18 18:05:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HighChart1::Application.config.secret_token = 'd3570664fa4b5d18f7c9f9bd407c5853636754b2438020923ea2533548ec543115c8cfb13e570f99b86437608814072cddb13a8357b92f55b50dc8e2d8ec9376'
